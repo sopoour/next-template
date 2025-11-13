730cb9e3a922e6f82fbc9cfe99eba310ba76a81a
sopoour <sophia.auer@googlemail.com>
2025-11-13 12:03:53 +0100

    updated formatDate

--- src/utils/formatDate.ts
+++ src/utils/formatDate.ts
@@ -28,3 +28,24 @@ export const dateToLocalDateType = (date: Date) => {
 
   return `${year}-${strMonth}-${strDay}`;
 };
+
+export const ISOToDate = (isoString: string) => {
+  return new Intl.DateTimeFormat('de-DE', {
+    day: '2-digit',
+    month: 'long',
+    year: 'numeric'
+  }).format(new Date(isoString));
+}
+
+export const ISOToTime = (isoString: string) => {
+  return new Intl.DateTimeFormat('de-DE', {
+    timeStyle: 'short',
+    timeZone: 'Europe/Berlin'
+  })?.format(new Date(isoString));
+}
+
+export const normalizeDate = (dateString: string) => {
+  const date = new Date(dateString);
+  date.setHours(0, 0, 0, 0); // Strip time
+  return date;
+};
